e1b573cb5f27462c0bc103adaeb3f31d87629c4c
OscarGonzalez14 <oscargonzalez815@gmail.com>
2022-11-13 12:21:33 -0800

    bugs imprimir citas 2

--- vistas/imprimir_citas_pdf.php
+++ vistas/imprimir_citas_pdf.php
@@ -99,7 +99,7 @@ $data = $citas->get_pacientes_citados($_POST["fecha-cita"],$sucursal);
     <td colspan="38" style="width: 25%;text-align: left;"><input type="text" class="input-report" value="Firma: "></td>
     <td colspan="37" style="width: 37%;text-align: left;"><input type="text" class="input-report" value="Sello: "></td>    
   </tr>
-
+</table>
 <table width="100%" id="tabla_reporte_citas" data-order='[[ 0, "desc" ]]' style="margin: 3px"  class="tabla_reporte_citas">        
  <tr>
    <th colspan="5" style="width:5%">#</th>
@@ -127,6 +127,58 @@ $data = $citas->get_pacientes_citados($_POST["fecha-cita"],$sucursal);
   <?php $i++; } ?>  
  </tbody>
 </table>
+
+<div style="page-break-after:always;"></div>
+
+<table style="width: 100%;margin-top:2px" width="100%">
+<td width="25%" style="width:10%;margin:0px">
+  <img src='../dist/img/inabve.jpg' width="90" height="70"/>
+</td>
+  
+<td width="60%" style="width:60%;margin:0px">
+<table style="width:100%">
+  <br>
+  <tr>
+    <td  style="text-align: center;margin-top: 0px;font-size:18px;font-family: Helvetica, Arial, sans-serif;"><b>CITAS DE PACIENTES - INAVBE (AGREGADAS)</b></td>
+  </tr>
+  <tr>
+    <td  style="text-align:center;margin-top:0px;font-size:14px;font-family: Helvetica, Arial, sans-serif;"><b>FECHA: <?php echo date("d-m-Y",strtotime($fecha_cita)); ?></b></td>
+  </tr>
+  <tr>
+    <td  style="text-align:center;margin-top:0px;font-size:15px;font-family: Helvetica, Arial, sans-serif;text-transform: uppercase"><u><b>SUCURSAL: <?php echo $sucursal?> </u></b></td>
+  </tr>
+</table>
+</td>
+
+</table>
+<table width="100%" i data-order='[[ 0, "desc" ]]' style="margin: 3px"  class="tabla_reporte_citas">        
+ <tr>
+  <th colspan="1" style="width:1%"></th>
+   <th colspan="9" style="width:9%">Hora</th> 
+   <th colspan="15" style="width:15%">DUI</th>
+   <th colspan="10" style="width:10%">Teléfono</th>   
+   <th colspan="30" style="width:30%">Nombre</th>
+   <th colspan="15" style="width:15%">Firma</th>
+   <th colspan="20" style="width:20%">Observaciones</th>
+ </tr>
+ <tbody class="style_th" style="font-size:11px">
+ <?php
+  //$j=1;
+  for ($j=1;$j<=25;$j++) { ?> 
+    <tr> 
+    <td colspan="1" style="padding:9px;width:1%;font-size:9px;color:#696969"><?php echo $i;?></td>
+     <td colspan="9" style="padding:0px;width:9%;text-align:left;font-size:9px;color:#696969"></td>
+     <td colspan="15" style="padding:9px;width:15%"></td>
+     <td colspan="10" style="padding:3px;width:10%"></td>
+     <td colspan="30" style="padding:3px;width:30%"></td>
+     <td colspan="15" style="padding:3px;width:15%"></td>
+     <td colspan="20" style="padding:10px;width:20%;color:white">I</td>
+    </tr> 
+
+  <?php $i++; } ?>  
+
+</table>
+
 </body>
 </html>
 
